cf31d2011eb92e4f6f76c96a5d8f456f89658b86
matheusmoc <matheusandrade009@gmail.com>
2025-10-05 16:51:34 -0300

    Refactor: routes to use CadastroController for cadastro-related endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CadastroController;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,13 @@ Route::get('/', function () {
 Route::middleware('web', 'auth')->group(function () {
     Route::get('/desafio-avelar', [Controller::class, 'index'])->name('desafio.avelar.index');
 });
+
+Route::get('/desafio-avelar', [CadastroController::class, 'index'])->name('desafio.avelar.index');
+Route::get('/', [CadastroController::class, 'index']);
+
+Route::get('/cadastro/listar', [CadastroController::class, 'listar']);
+Route::get('/cadastro/estatisticas', [CadastroController::class, 'estatisticas']);
+Route::post('/cadastro', [CadastroController::class, 'store']);
+Route::get('/cadastro/{id}', [CadastroController::class, 'show']);
+Route::put('/cadastro/{id}', [CadastroController::class, 'update']);
+Route::delete('/cadastro/{id}', [CadastroController::class, 'destroy']);
